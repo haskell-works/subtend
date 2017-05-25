4865441633dc6cad350e16e5236e53493d1be789
John Ky <newhoggy@gmail.com>
2017-05-25 18:49:19 +1000

    Drill into dialogues

--- src/Subtend/Conversion.hs
+++ src/Subtend/Conversion.hs
@@ -9,26 +9,27 @@ import Subtend.Ast.Srt    as Srt
 import Subtend.Data.List
 import Subtend.Data.ToMap
 
+import qualified Data.Text as T
+
 buildSrt :: [Map String String] -> Either String Srt.Document
 buildSrt = undefined
 
--- groupBy :: Eq k => (a -> k) -> (a -> v) -> [a] -> Map k [v]
--- groupBy fk fv (a:as) = unionWith (++)
--- groupBy _ _ []       = empty
+asTime :: String -> Maybe Srt.Time
+asTime = undefined
 
-formatAndDialogueToSrtEntries :: Values -> Values -> Srt.Entry
-formatAndDialogueToSrtEntries = undefined
+formatAndDialogueToSrtEntries :: Values -> Values -> Maybe Srt.Entry
+formatAndDialogueToSrtEntries (Values ks) (Values vs) = Srt.Entry 0 <$> frame <*> text
+  where kvs   = M.fromList (ks `zip` vs)
+        frame = Srt.Frame
+                <$> (M.lookup "Start" kvs >>= asTime)
+                <*> (M.lookup "Stop"  kvs >>= asTime)
+        text  = (:[]) . T.pack <$> M.lookup "Text" kvs
 
 convert :: Ass.Document -> Either String Srt.Document
 convert (Ass.Document sections) = case M.lookup "Events" (toMap sections) of
   Just entries -> case toMap entries of
     entryMap -> case M.lookup "Format" entryMap >>= listToMaybe of
       Just format -> case M.lookup "Dialogue" entryMap of
-        Just dialogues -> Right (Srt.Document (formatAndDialogueToSrtEntries format <$> dialogues))
+        Just dialogues -> Right (Srt.Document (catMaybes (formatAndDialogueToSrtEntries format <$> dialogues)))
       Nothing -> Left "'Format' entry missing"
   Nothing -> Left "[Events] section missing"
-  -- case find (\s -> Ass.name s == "Events") sections of
-  --   Just section -> case Ass.entries section of
-  --     entries -> let x = toMap Ass.key Ass.values entries in undefined
-  --     []      -> Left "No header"
-  --   Nothing      -> Left "Section missing"
